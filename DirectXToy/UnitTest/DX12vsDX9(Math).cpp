93a0d2be9c447c698b0644432b8d9ccf20d20608
parkminuk <pmw0425@estgames.com>
2023-01-19 14:08:23 +0900

    UnitTest 보기 깔끔하게 수정

--- DirectXToy/UnitTest/DX12vsDX9(Math).cpp
+++ DirectXToy/UnitTest/DX12vsDX9(Math).cpp
@@ -3,6 +3,7 @@
 #include <D3dx9math.h>
 #include <iostream>
 #include <chrono>
+#include <iomanip>
 
 #pragma comment(lib, "D3dx9.lib")
 
@@ -10,17 +11,18 @@ using namespace DirectX;
 using namespace DirectX::PackedVector;
 
 
-bool Test1(XMFLOAT4X4& left, CONST XMFLOAT4X4& right)
+bool Test1(XMFLOAT4X4& left, XMFLOAT4X4& right)
 {
-	XMStoreFloat4x4(&left, XMMatrixInverse(nullptr, XMLoadFloat4x4(&left)));
+	XMFLOAT4X4 out;
+	XMStoreFloat4x4(&out, XMMatrixMultiply(XMLoadFloat4x4(&left), XMLoadFloat4x4(&right)));
 
 	return true;
 }
 
-
-bool Test2(D3DXMATRIX& left, CONST D3DXMATRIX& right)
+bool Test2(D3DXMATRIX& left, D3DXMATRIX& right)
 {
-	D3DXMatrixInverse(&left, nullptr, &left);
+	D3DXMATRIX out;
+	D3DXMatrixMultiply(&out, &left, &right);
 
 	return true;
 }
@@ -28,7 +30,6 @@ bool Test2(D3DXMATRIX& left, CONST D3DXMATRIX& right)
 D3DXMATRIX* D3DXMatrixInverse2(D3DXMATRIX* pout, FLOAT* pdeterminant, CONST D3DXMATRIX* pm);
 bool Test3(D3DXMATRIX& left, CONST D3DXMATRIX& right)
 {
-	D3DXMatrixInverse2(&left, nullptr, &left);
 
 	return true;
 }
@@ -105,12 +106,23 @@ D3DXMATRIX* D3DXMatrixInverse2(D3DXMATRIX* pout, FLOAT* pdeterminant, CONST D3DX
 
 int main()
 {
-	XMFLOAT4X4 a
-	{
+#ifdef _X86_
+	std::cout << "X86" << std::endl;
+#else
+	std::cout << "X64" << std::endl;
+#endif
+	/*
 		1,0,0,0,
 		0,4,5,0,
 		0,1,1,0,
 		0,0,0,1
+	*/
+	XMFLOAT4X4 a
+	{
+		1,5,0,2,
+		0,4,5,0,
+		2,1,1,0,
+		6,0,0,1
 	};
 	XMFLOAT4X4 b
 	{
@@ -122,10 +134,10 @@ int main()
 
 	D3DXMATRIX aa
 	{
-		1,0,0,0,
+		1,5,0,2,
 		0,4,5,0,
-		0,1,1,0,
-		0,0,0,1
+		2,1,1,0,
+		6,0,0,1
 	};
 	D3DXMATRIX bb
 	{
@@ -143,7 +155,7 @@ int main()
 		Test1(a, b);
 	}
 	auto end = std::chrono::steady_clock::now();
-	std::cout << "시간 : " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
+	std::cout << "DX12 : " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
 
 	begin = std::chrono::steady_clock::now();
 	for (long long i{}; i < loop; ++i)
@@ -151,15 +163,15 @@ int main()
 		Test2(aa, bb);
 	}
 	end = std::chrono::steady_clock::now();
-	std::cout << "시간 : " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
+	std::cout << "DX 9 : " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
 
-
+	/*
 	begin = std::chrono::steady_clock::now();
 	for (long long i{}; i < loop; ++i)
 	{
 		Test3(aa, bb);
 	}
 	end = std::chrono::steady_clock::now();
-	std::cout << "시간 : " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
-
+	std::cout << "CM   : " <<std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
+	*/
 }
